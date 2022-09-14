610cbb278cce20622dff20626fea2118cc8534fb
Kelvin Chao <kelvinchao2011@gmail.com>
2022-09-13 23:48:36 -0700

    working on weather

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css'
+import "./App.css";
 import Search from "./components/Search/search";
-import CurrentWeather from "./components/Current-Weather/current-weather"
+import CurrentWeather from "./components/Current-Weather/current-weather";
 
 function App() {
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData)
-  }
-  
+    console.log(searchData);
+  };
+
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange}/>
+      <Search onSearchChange={handleOnSearchChange} />
       <CurrentWeather />
     </div>
   );
--- src/components/Current-Weather/current-weather.js
+++ src/components/Current-Weather/current-weather.js
@@ -1,15 +1,41 @@
-import './current-weather.css'
+import "./current-weather.css";
 
 const CurrentWeather = () => {
   return (
-    <div className='weather'>
+    <div className="weather">
       <div className="top">
-        <p className='city'>Belgrade</p>
-        <p className='weather-description'>Sunny</p>
+        <div>
+          <p className="city">Belgrade</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/cloud.png"></img>
       </div>
-      <img alt='weather' className='weather-icon' src='icons/xyz.png'></img>
+      <div className="bottom">
+        <p className="temperature">20 C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels Like</span>
+            <span className="parameter-value">22 C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">20%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">10 hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CurrentWeather
+export default CurrentWeather;
