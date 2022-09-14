ed076ae1154bfc86d87ca2c6d432e1a39a0505b3
Kelvin Chao <kelvinchao2011@gmail.com>
2022-09-14 00:05:37 -0700

    .

--- src/api.js
+++
@@ -1,9 +0,0 @@
-export const geoApiOptions = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "1096f0119dmshcfc5a1e06a141efp1bb492jsnf69308761e2f",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-  },
-};
-
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
