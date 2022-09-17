8120aabaa5f6f01332d9730a1f7065fab7ec1bcd
Kelvin Chao <kelvinchao2011@gmail.com>
2022-09-16 20:18:10 -0700

    hide api key

---
+++ src/api_replace.js
@@ -0,0 +1,17 @@
+const GEO_API = process.env.REACT_APP_API_KEY
+const WEATHER_API = process.env.WEATHER_API_KEY
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": GEO_API,
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = WEATHER_API
